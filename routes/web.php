782b9e233f9fd97b668880cc4bb0a10114496f3f
Rhivandemario54 <rhivandemario54@gmail.com>
2023-08-18 08:42:36 +0700

    Tahap Pertama

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,26 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Rhivan Gabrille Demarrio",
+        "email" => "rhivandemario54@gmail.com",
+        "image" => "profile.JPEG"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
+});
+Route::get('/karyawan', function () {
+    return view('karyawan');
 });
