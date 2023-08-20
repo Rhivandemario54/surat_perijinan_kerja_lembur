d8bcd82150e21b77a171149cd35c9475c0ed42e6
Rhivandemario54 <rhivandemario54@gmail.com>
2023-08-20 20:32:01 +0700

    Tahap Ketujuh

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.main')
 
 @section('container')
-  <article>
-    <h2>{{ $post->tittle }}</h2>
-    <p>By. in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
-    {!! $post->body !!}
-  </article>
-  <a href="/blog">Back to Posts</a>s
+        <Article>
+            <h2>{{ $post->title }}</h2>
+            <p>By. <a href="#"class='text-decoration-none'>{{ $post->user->name }}</a>in <a href="/categories/{{ $post->category->slug }}"class='text-decoration-none'>{{ $post->category->name }}</a></p>
+            {!! $post->body !!}
+        </Article>
+        <a href="/blog"class='d-block mt-2'>Back</a>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::get('/karyawan', function () {
 
 Route::get('/categories', function () {
     return view('categories', [
-        'tittle' => 'Post Categories',
+        'title' => 'Post Categories',
         'categories' => Category::all()
     ]);
 });
@@ -54,7 +54,7 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('category', [
-        'tittle' => $category->name,
+        'title' => $category->name,
         'posts' => $category->posts,
         'category' => $category->name
     ]);
